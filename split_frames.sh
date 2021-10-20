8edd66c307d1ac10ca7db22f65c55e659a3b5a7d
danofsteel32 <dan@dandavis.dev>
2021-10-19 21:02:13 -0400

    pos and neg splits

--- split_frames.sh
+++ split_frames.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# -ss start at 680 seconds (11m 30s)
-# -sseof end at 12 minutes
+mkdir -p data/{pos,neg}
+
+# -t total duration in seconds
 # -vsync 0 all frames
-ffmpeg -ss 680 \
-       -sseof 720 \
-       -threads 4 \
+
+# Positives (11m - 11m39s)
+ffmpeg -ss 660 \
+       -t 39 \
        -i data/ProgDieFeed_20211014000000.avi \
        -vsync 0 \
        -frame_pts true \
-       -f image2 'data/frames/img-%d.png'
+       -f image2 'data/pos/pos-%d.png'
 
-
+# Negatives (11m40s - 11m55s)
+ffmpeg -ss 700 \
+       -t 15 \
+       -i data/ProgDieFeed_20211014000000.avi \
+       -vsync 0 \
+       -frame_pts true \
+       -f image2 'data/neg/neg-%d.png'
